01416828ed913ff6cb0869b2b2b7badb8518cb11
mizdra <pp.mizdra@gmail.com>
2018-03-16 01:36:36 +0900

    refactor: create rand module

--- src/wasm/lib.rs
+++ src/wasm/lib.rs
@@ -1,21 +1,4 @@
 extern crate tinymt;
 
 pub mod math;
-
-use tinymt::tinymt32;
-
-#[no_mangle]
-pub fn gen_by_index(seed: u32, index: usize) -> u32 {
-  let param = tinymt32::Param {
-    mat1: 0x8F7011EE,
-    mat2: 0xFC78FF1F,
-    tmat: 0x3793fdff,
-  };
-  let seed = seed;
-
-  let mut rng = tinymt32::from_seed(param, seed);
-  for _i in 0..index {
-    rng.next_state()
-  }
-  rng.temper()
-}
+pub mod rand;
