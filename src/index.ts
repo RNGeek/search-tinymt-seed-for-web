4600c35c84fcf98879459368f48d823e9182b94a
mizdra <pp.mizdra@gmail.com>
2018-03-16 00:57:58 +0900

    feat: add tinymt

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-import { add } from './wasm/lib.rs'
+import { add, gen_by_index } from './wasm/lib.rs'
 
-console.log(add(1, 2))
+const seed = 1
+const index = 50
+const sum = add(1, 2)
+const rand = gen_by_index(seed, index)
+
+console.assert(sum === 3, `${sum} !== 3`)
+console.assert((rand >>> 0) === 2292524454, `(${rand} >>> 0) !== 2292524454`)
--- src/wasm/lib.rs.d.ts
+++ src/wasm/lib.rs.d.ts
@@ -1,1 +1,2 @@
 export function add(a: number, b: number): number
+export function gen_by_index(seed: number, index: number): number
