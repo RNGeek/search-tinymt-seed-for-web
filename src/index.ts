53fe0e0a472b3f87cbfac4f9150ba3ac96418d1b
mizdra <pp.mizdra@gmail.com>
2018-03-16 13:09:47 +0900

    refactor: use cast function

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { add, gen_by_index } from './wasm/lib.rs'
 
+const u32 = (num: number) => num >>> 0
+
 const seed = 1
 const index = 50
+
 const sum = add(1, 2)
 const rand = gen_by_index(seed, index)
 
-console.assert(sum === 3, `${sum} !== 3`)
-console.assert((rand >>> 0) === 2292524454, `(${rand} >>> 0) !== 2292524454`)
+console.assert(u32(sum) === 3, `u32(${sum}) !== 3`)
+console.assert(u32(rand) === 2292524454, `u32(${rand}) !== 2292524454`)
