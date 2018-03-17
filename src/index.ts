e5065cbeb908718c9c3cd8196bb92584975c955a
mizdra <pp.mizdra@gmail.com>
2018-03-18 00:03:33 +0900

    feat: implement search_tinymt_seed prototype

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-import { add, gen_by_index } from './wasm/lib.rs'
+import { add, gen_by_index, search_tinymt_seed } from './wasm/lib.rs'
 
 const u32 = (num: number) => num >>> 0
 
-const seed = 1
-const index = 50
-
 const sum = add(1, 2)
-const rand = gen_by_index(seed, index)
+const rand = gen_by_index(1, 50)
 
 console.assert(u32(sum) === 3, `u32(${sum}) !== 3`)
 console.assert(u32(rand) === 2292524454, `u32(${rand}) !== 2292524454`)
+
+console.time('search_tinymt_seed')
+const seed = search_tinymt_seed(18, 2, 12, 16, 19, 19, 20, 7, false)
+console.timeEnd('search_tinymt_seed')
+console.log(`seed == 0x${u32(seed).toString(16).toUpperCase()}`)
--- src/wasm/rand.rs
+++ src/wasm/rand.rs
@@ -1,5 +1,22 @@
 use tinymt::tinymt32;
 
+macro_rules! each_u32 {
+  ($var:ident => $code:block) => {
+    each_u32!(0x0000_0000, 0xFFFF_FFFF, $var => $code)
+  };
+  ($from:expr, $to:expr, $var:ident => $code:block) => {
+    let mut count: u32 = $from;
+    loop {
+      let $var: u32 = count;
+      { $code }
+      if count == $to {
+        break;
+      }
+      count += 1;
+    }
+  };
+}
+
 #[no_mangle]
 pub fn gen_by_index(seed: u32, index: usize) -> u32 {
   let param = tinymt32::Param {
@@ -14,4 +31,81 @@ pub fn gen_by_index(seed: u32, index: usize) -> u32 {
     rng.next_state()
   }
   rng.temper()
+}
+
+fn get_egg_nature(rng: &mut tinymt32::Rng, has_shiny_charm: bool) -> u32 {
+  rng.next_state();            // gender
+  let nature = rng.gen() % 25; // nature
+  rng.next_state();            // ability
+
+  let mut inherit = [false; 6];
+
+  for _i in 0..3 {
+    let mut r = (rng.gen() % 6) as usize;
+    while inherit[r] {
+      r = (rng.gen() % 6) as usize;
+    }
+    inherit[r] = true;
+    rng.next_state();
+  }
+
+  for _i in 0..6 {
+    rng.next_state(); // IVs
+  }
+
+  rng.next_state(); // EC
+
+  if has_shiny_charm {
+    for _i in 0..2 {
+      rng.next_state();
+    }
+  }
+
+  // rng.next_state(); // ball
+  rng.next_state(); // unknown
+  rng.next_state(); // unknown
+
+  nature
+}
+
+#[no_mangle]
+pub fn search_tinymt_seed(
+  nature1: u32,
+  nature2: u32,
+  nature3: u32,
+  nature4: u32,
+  nature5: u32,
+  nature6: u32,
+  nature7: u32,
+  nature8: u32,
+  has_shiny_charm: bool,
+) -> u32 {
+  let natures = [
+    nature1,
+    nature2,
+    nature3,
+    nature4,
+    nature5,
+    nature6,
+    nature7,
+    nature8,
+  ];
+
+  each_u32!(seed => {
+    let param = tinymt32::Param {
+      mat1: 0x8F7011EE,
+      mat2: 0xFC78FF1F,
+      tmat: 0x3793FDFF,
+    };
+    let mut rng = tinymt32::from_seed(param, seed);
+
+    let found = natures
+      .iter()
+      .all(|&nature| nature == get_egg_nature(&mut rng, has_shiny_charm));
+
+    if found {
+      return seed;
+    }
+  });
+  0xFFFF_FFFF
 }
