d97820ab854c67bbe48bcaab2694f74d3801ec51
mizdra <pp.mizdra@gmail.com>
2020-01-03 21:52:51 +0900

    refactor: remove unused files

--- src/wasm/calculator.d.ts
+++
@@ -1,8 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-/**
-* @param {Uint32Array} natures 
-* @param {boolean} has_shiny_charm 
-* @returns {Uint32Array} 
-*/
-export function search_tinymt_seed(natures: Uint32Array, has_shiny_charm: boolean): Uint32Array;
